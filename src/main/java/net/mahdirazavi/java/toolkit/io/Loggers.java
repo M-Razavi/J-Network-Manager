bc270f484ca69b8dde35679cbff98714f6df8ac5
Mahdi Razavi <mahdi.razavi@webbfontaine.com>
2020-02-04 14:28:15 +0400

    Migrate to log4j2
    Took 27 minutes

--- src/main/java/net/mahdirazavi/java/toolkit/io/Loggers.java
+++ src/main/java/net/mahdirazavi/java/toolkit/io/Loggers.java
@@ -1,41 +1,39 @@
 package net.mahdirazavi.java.toolkit.io;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
 
 /**
  * The Class Loggers.
- *
  */
 public class Loggers {
 
-  // sorted alphabetically
+    // sorted alphabetically
 
-  /** The Constant Algorithm (section based logger). */
-  public static final Logger Algorithm = Logger.getLogger("alg");
+    /**
+     * The Constant Algorithm (section based logger).
+     */
+    public static final Logger Algorithm = LogManager.getLogger("alg");
 
-  /** The Constant Application (section based logger). */
-  public static final Logger Application = Logger.getLogger("app");
+    /**
+     * The Constant Application (section based logger).
+     */
+    public static final Logger Application = LogManager.getLogger("app");
 
-  /** The Constant Network (section based logger). */
-  public static final Logger Network = Logger.getLogger("net");
+    /**
+     * The Constant Network (section based logger).
+     */
+    public static final Logger Network = LogManager.getLogger("net");
 
-  /** The Constant Packet (section based logger). */
-  public static final Logger Packet = Logger.getLogger("pkt");
+    /**
+     * The Constant Packet (section based logger).
+     */
+    public static final Logger Packet = LogManager.getLogger("pkt");
 
-  /** The Constant PacketFlow (logic based logger). */
-  public static final Logger PacketFlow = Logger.getLogger("flow");
-
-
-  static {
-    DateFormat dateFormat = new SimpleDateFormat("(yyyy-MM-dd)HH-mm-ss");
-    Date date = new Date();
-    System.setProperty("logfilename", dateFormat.format(date));
-    DOMConfigurator.configure("config/loggers.xml");
-  }
+    /**
+     * The Constant PacketFlow (logic based logger).
+     */
+    public static final Logger PacketFlow = LogManager.getLogger("flow");
 
 }
